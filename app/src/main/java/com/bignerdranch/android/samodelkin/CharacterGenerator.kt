ff2740f401a528c2e74fa248723f3a4025ed38e5
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-11 20:03:39 +0200

    Add CharacterGenerator object

--- app/src/main/java/com/bignerdranch/android/samodelkin/CharacterGenerator.kt
+++ app/src/main/java/com/bignerdranch/android/samodelkin/CharacterGenerator.kt
@@ -7,3 +7,36 @@ package com.bignerdranch.android.samodelkin
  * http://bignerdranch.com
  */
 
+private fun <T> List<T>.rand() = shuffled().first()
+
+private fun Int.roll() = (0 until this)
+        .map { (1..6).toList().rand() }
+        .sum()
+        .toString()
+
+private val firstName = listOf("Eli, Alex, Sophie")
+private val lastName = listOf("Lightwear", "Greatfoot", "Oakeanfield")
+
+object characterGenerator {
+    data class CharacterData(val name: String,
+                             val race: String,
+                             val dex: String,
+                             val wis: String,
+                             val str: String)
+
+    private fun name() = "${firstName.rand()} ${lastName.rand()}"
+
+    private fun race() = listOf("dwarf", "elf", "human", "halflin").rand()
+
+    private fun dex() = 4.roll()
+
+    private fun wis() = 3.roll()
+
+    private fun str() = 5.roll()
+
+    fun generate() = CharacterData(name = name(),
+                                    race = race(),
+                                    dex = dex(),
+                                    wis = wis(),
+                                    str= str())
+}
