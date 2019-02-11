5614b1d93a4f242e2537626e9b215197aacff1b3
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-11 19:57:36 +0200

    Add empty CharacterGenerator class

---
+++ app/src/main/java/com/bignerdranch/android/samodelkin/CharacterGenerator.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.samodelkin
+
+/**
+ * Created by Riku Pepponen on 11.2.2019.
+ * (riku.pepponen@gmail.com)
+ * Thanks to the Big Nerd Ranch!
+ * http://bignerdranch.com
+ */
+
