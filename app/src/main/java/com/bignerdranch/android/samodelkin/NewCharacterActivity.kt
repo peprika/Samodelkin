37cc8ea9a2a6d2c3a623a30d6b9d902399590ee3
Riku Pepponen <riku.pepponen@gmail.com>
2019-02-13 16:41:43 +0200

    Create extension for handling character data

--- app/src/main/java/com/bignerdranch/android/samodelkin/NewCharacterActivity.kt
+++ app/src/main/java/com/bignerdranch/android/samodelkin/NewCharacterActivity.kt
@@ -7,20 +7,23 @@ import kotlinx.android.synthetic.main.activity_new_character.*
 
 private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
 
+private  var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
+
 class NewCharacterActivity : AppCompatActivity() {
     private var characterData = CharacterGenerator.generate()
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        outState.putSerializable(CHARACTER_DATA_KEY, characterData)
+        outState.characterData = characterData
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_character)
 
-        characterData = savedInstanceState?.let {
-            it.getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
-        } ?: CharacterGenerator.generate()
+        characterData = savedInstanceState?.characterData ?:
+            CharacterGenerator.generate()
 
         generateButton.setOnClickListener {
             characterData = CharacterGenerator.generate()
